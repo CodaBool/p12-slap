14045612c03b294b7637d4abe9739142dc731d99
Coda Bool <codabool@pm.me>
2023-01-11 22:47:24 -0500

    force reading env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withTM = require('next-transpile-modules')(['three'])
 module.exports = withTM({
   reactStrictMode: false, // stricter checks, https://reactjs.org/docs/strict-mode.html
-  swcMinify: true, // Rust based compiler, replaces Babel
 })
